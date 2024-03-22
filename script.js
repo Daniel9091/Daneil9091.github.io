0c9c2299e50beb6ddf19edf214e3b54f906c5f13
Daniel9091 <djguzman1@miuandes.cl>
2024-03-22 14:59:40 -0300

    entrea 2

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function cambiar_contenido() {
     var n_mensaje = document.getElementById("nuevo_mensaje").value;
     document.getElementById("texto").textContent = n_mensaje;
+    document.getElementById("alerta").style.display = "block";
 }
